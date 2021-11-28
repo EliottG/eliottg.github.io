c40daf98aa53ecaae6d9867c19b0da0b4f830017
EliottG <eliott.geoffroy@gmail.com>
2021-11-28 22:08:17 +0100

    push

--- public/script/index.js
+++ public/script/index.js
@@ -1,11 +1,14 @@
 function updateAnimations() {
   let anim = document.querySelector('header > h1');
+  let animBorder = document.querySelector('section > div.about-me');
   if (anim.classList.contains('opacity-anim')) {
     document.getElementById('animations-disable').innerText =
       'Enable animations';
     anim.classList.remove('opacity-anim');
+    animBorder.classList.remove('light-border');
   } else {
     anim.classList.add('opacity-anim');
+    animBorder.classList.add('light-border');
     document.getElementById('animations-disable').innerText =
       'Disable animations';
   }
